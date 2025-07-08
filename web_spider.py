01714d4780e519861f66cd10e8f2b3c44fd7930b
antirahat <rahatdhrubo123@gmail.com>
2025-07-09 04:10:30 +0600

    crawler working

--- web_spider.py
+++ web_spider.py
@@ -1,7 +1,8 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service  
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By  # Added import
 
 chrome_options = Options()
 chrome_options.add_argument("--incognito")
@@ -11,7 +12,26 @@ service = Service('./chromedriver-win64/chromedriver.exe')
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-driver.get("https://en.prothomalo.com/")
-time.sleep(5)  # wait for page to load
-print(driver.title)
+# Fetch the URL
+url = "https://en.prothomalo.com/search?q=accident"
+driver.get(url)
+time.sleep(3)
+
+articles = driver.find_elements(By.CSS_SELECTOR, "div.K-MQV a")
+printed = 0
+printed_links = set()  
+
+for article in articles:
+    href = article.get_attribute("href")
+    title = article.text.strip()
+
+    if href and title and href not in printed_links:
+        print(f"{printed + 1}. {title}")
+        print(f"   🔗 {href}\n")
+        printed_links.add(href)
+        printed += 1
+
+    if printed == 5:
+        break
+
 driver.quit()
